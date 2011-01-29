957c06e58c43de03a07dd0c407309c85d2d59125
Tom Aldcroft <aldcroft@head.cfa.harvard.edu>
2011-01-29 10:59:43 -0500

    Lots of updates, good working version

--- esaview.py
+++ esaview.py
@@ -12,7 +12,7 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 import Ska.quatutil
 from Chandra.Time import DateTime
-
+from Ska.Matplotlib import plot_cxctime, cxctime2plotdate
 
 def destroy(e): sys.exit()
 
@@ -22,37 +22,86 @@ def destroy(e): sys.exit()
 
 def get_index_lims():
     center = int(date_slider.value.get())
-    wid = int(width_slider.value.get() * 6)
+    wid = int(width_slider.value.get() * 3)
     i0 = center - wid
     i1 = center + wid
     if i0 < 0:
         i0 = 0
     if i1 >= len(imgs):
-        i1 = len(imgs)-1
-    return i0, i1
+        i1 = len(imgs) - 1
+    return i0, i1, center
 
 def update_image(*args):
-    i0, i1 =get_index_lims()
+    i0, i1, i_center = get_index_lims()
     img = imgs[i0:i1+1].sum(0)
-    img_rgba = matplotlib.cm.jet(img / 7.2)
+    img_rgba = matplotlib.cm.spectral(img / 7.2)
     img_rgba[:, :, 3] = alpha_slider.value.get()
     image.set_data(img_rgba)
+    ax.set_title(get_date(i_center))
     canvas.draw()
 
-def motion(event):
-    if event.inaxes is None:
-        return
-    x = event.xdata
-    y = event.ydata
-    # print antisun.img2sky(x, y, sun_eci)
+
+class ImageCoords(object):
+    def __init__(self):
+        self.frame = Tk.Frame()
+        self.textvar = dict()
+        self.textvar['ra'] = Tk.StringVar()
+        self.textvar['dec'] = Tk.StringVar()
+        self.textvar['pitch'] = Tk.StringVar()
+        self.textvar['phi'] = Tk.StringVar()
+        Tk.Label(self.frame, text='RA, Dec').grid(row=0)
+        Tk.Label(self.frame, text='Pitch, Phi').grid(row=1)
+        self.ra = Tk.Label(self.frame, textvariable=self.textvar['ra'], width=12)
+        self.dec = Tk.Label(self.frame, textvariable=self.textvar['dec'], width=12)
+        self.pitch = Tk.Label(self.frame, textvariable=self.textvar['pitch'])
+        self.phi = Tk.Label(self.frame, textvariable=self.textvar['phi'])
+        self.ra.grid(row=0, column=1)
+        self.dec.grid(row=0, column=2)
+        self.pitch.grid(row=1, column=1)
+        self.phi.grid(row=1, column=2)
     
+    def update(self, event):
+        if event.inaxes != ax:
+            return
+        x = event.xdata
+        y = event.ydata
+        ra, dec = antisun.img2sky(x, y, sun_eci)
+        r, phi = antisun.img2polar(x, y)
+        pitch = 180 - r
+        self.textvar['ra'].set('{0:.4f}'.format(ra))
+        self.textvar['dec'].set('{0:.4f}'.format(dec))
+        self.textvar['pitch'].set('{0:.1f}'.format(pitch))
+        self.textvar['phi'].set('{0:.1f}'.format(np.degrees(phi)))
+
+class TimePlot(object):
+    def __init__(self, fig, rect, times, ephem_xyzs):
+        self.ax = fig.add_axes(rect)
+        orbit_rs = np.sqrt(np.sum(ephem_xyzs['earth']**2, 0))
+        plot_cxctime(times, orbit_rs, fig=fig, ax=self.ax)
+        self.ax.grid()
+        self.ax.set_autoscale_on(False)
+        self.update()
+
+    def update(self, *args):
+        pd0, pd1, pd_center = cxctime2plotdate(times[np.array(get_index_lims())])
+        if not hasattr(self, 'patch'):
+            self.patch = matplotlib.patches.Rectangle(
+                (pd0, 0), width=(pd1-pd0), height=1.6e8, zorder=-100,
+                facecolor='y', alpha=0.5)
+            self.ax.add_patch(self.patch)
+        else:
+            self.patch.set_xy((pd0, 0))
+            self.patch.set_width(pd1-pd0)
+
 class SolarSystemObject(object):
-    def __init__(self, name, times, xyzs, color, npoly=40):
+    def __init__(self, name, times, xyzs, color, ax, npoly=40):
         self.name = name
         self.times = times
         self.xyzs = xyzs
         self.color = color
-        self.patches = []
+        self.ax = ax
+        self.regions = {}
+        self.idxs_visible = set()
 
         for idx in range(len(times)):
             xyz = ephem_xyzs[name][:, idx]
@@ -67,23 +116,39 @@ class SolarSystemObject(object):
             quat_x_to_obj = Ska.quatutil.quat_x_to_vec(xyz)
             obj_ecis = np.dot(quat_x_to_obj.transform, ecis)
             x, y = antisun.eci2img(obj_ecis, sun_eci)
-            line = ax.plot(x, y, color=color, visible=True, alpha=0.1)[0]
-            #patch = matplotlib.patches.Polygon(
-            #    np.array([x,y]).transpose(), edgecolor=color, facecolor='none',
-            #    fill=False, visible=True, alpha=0.1)
-            # ax.add_patch(patch)
-            self.patches.append(line)
+            self.regions[idx] = dict(x=x, y=y, linewidth=1)
 
     def update(self, *args):
-        i0, i1 = get_index_lims()
-        for i, patch in enumerate(self.patches):
-            if i0 <= i <= i1:
-                patch.set_visible(True)
-                alpha = 1.0 - abs(i - (i0+i1)/2) / ((i1-i0+1.0)/2.0)
-                print alpha
-                patch.set_alpha(alpha)
-            else:
-                patch.set_visible(False)
+        i0, i1, i_center = get_index_lims()
+        stride = (i1 - i0) / 20
+        if stride == 0:
+            stride = 1
+        idxs = np.arange(0, len(self.times), stride)
+        idx_center = idxs[np.argmin(np.abs(idxs - i_center))]
+        idxs = set(idxs[(idxs >= i0) & (idxs <= i1)])
+        # Disable regions that are currently visible but not in next view
+        for idx in self.idxs_visible - idxs:
+            self.regions[idx]['line'].set_visible(False)
+            
+        for idx in idxs - self.idxs_visible:
+            region = self.regions[idx]
+            try:
+                region['line'].set_visible(True)
+            except KeyError:
+                region['line'] = self.ax.plot(region['x'], region['y'],
+                                              color=self.color, visible=True)[0]
+
+        self.idxs_visible = idxs
+
+#         for i, line in enumerate(self.lines):
+#             if i0 <= i <= i1:
+#                 line.set_visible(True)
+#                 if i == i_center:
+#                     line.set_linewidth(2.0)
+#                 else:
+#                     line.set_linewidth(1.0)
+#             else:
+#                 line.set_visible(False)
                                                
 class Slider(object):
     def __init__(self, minval, maxval, label_command=None, side=Tk.TOP, anchor='w', **kwargs):
@@ -120,13 +185,15 @@ def get_date(idx_img):
         idx_img = len(times) - 1
     return DateTime(times[idx_img]).date[:-4]
 
-# Load data
-dat = pickle.load(open('cube3.pkl'))
+# Load data and set some globals (hopefully minimize this later)
+filename = sys.argv[1]
+dat = pickle.load(open(filename))
 imgs = dat['illums']
 times = dat['times']
 antisun = dat['antisun']
 sun_eci = dat['sun_eci']
 ephem_xyzs = dat['ephem_xyzs']
+
 # Ephemerides in frame aligned with ECI but centered on Chandra
 ephem_xyzs['sun'] = ephem_xyzs['solar'] - ephem_xyzs['orbit']
 ephem_xyzs['earth'] = -ephem_xyzs['orbit']
@@ -142,8 +209,14 @@ root = Tk.Tk()
 root.wm_title("ESA viewer")
 root.bind("<Destroy>", destroy)
 
-fig = Figure(figsize=(8,7), dpi=100)
-ax = fig.add_subplot(1, 1, 1, axis_bgcolor='k')
+matplotlib.rc("axes", labelsize=10)
+matplotlib.rc("xtick", labelsize=10)
+matplotlib.rc("ytick", labelsize=10)
+
+fig = Figure(figsize=(8,9), dpi=100)
+ax = fig.add_axes([0.1, 0.25, 0.7, 0.7], axisbg='b')
+ax.format_coord = lambda x,y: ""
+
 #ax.set_xticklabels([])
 #ax.set_yticklabels([])
 
@@ -159,8 +232,6 @@ canvas = FigureCanvasTkAgg(fig, master=image_frame)
 canvas.show()
 canvas.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
 
-canvas.mpl_connect('motion_notify_event', motion)
-
 toolbar = NavigationToolbar2TkAgg(canvas, image_frame)
 toolbar.update()
 canvas._tkcanvas.pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
@@ -169,12 +240,12 @@ canvas._tkcanvas.pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
 maxscale = 0.4 * 3 * 6   # illum = 3 hours at 0.4
 image = ax.imshow(imgs[0], interpolation='bilinear', animated=True, vmin=0,
                   vmax=maxscale, alpha=1.0, origin='lower')
-image.set_cmap('jet')
+image.set_cmap('spectral')
 ax.set_autoscale_on(False)
 
 # Make / draw Earth and Moon constraints
-earth = SolarSystemObject('earth', times, ephem_xyzs, color='r')
-moon = SolarSystemObject('moon', times, ephem_xyzs, color='y')
+earth = SolarSystemObject('earth', times, ephem_xyzs, color='r', ax=ax)
+moon = SolarSystemObject('moon', times, ephem_xyzs, color='y', ax=ax)
 
 # Make colorbar
 divider = make_axes_locatable(ax)
@@ -182,7 +253,7 @@ cax = divider.append_axes("right", size="5%", pad=0.05)
 fig.colorbar(image, cax=cax)
 
 # Date slider control
-date_slider = Slider(minval=0, maxval=len(imgs), label_command=get_date, length=300)
+date_slider = Slider(minval=0, maxval=len(imgs)-1, label_command=get_date, length=500)
 date_slider.value.trace('w', earth.update)
 date_slider.value.trace('w', moon.update)
 date_slider.value.trace('w', update_image)
@@ -200,13 +271,15 @@ alpha_slider = Slider(minval=0.0, maxval=1.0, resolution=0.01,
 alpha_slider.value.set(1.0)
 alpha_slider.value.trace('w', update_image)
 
+time_plot = TimePlot(fig, [0.1, 0.05, 0.7, 0.15], times, ephem_xyzs)
+date_slider.value.trace('w', time_plot.update)
+width_slider.value.trace('w', time_plot.update)
+
+image_coords = ImageCoords()
+image_coords.frame.pack()
+canvas.mpl_connect('motion_notify_event', image_coords.update)
+
 update_image(None)
+earth.update()
+moon.update()
 Tk.mainloop()
-
-
-#sfreq.on_changed(update)
-#samp.on_changed(update)
-
-#rax = axes([0.025, 0.5, 0.15, 0.15], axisbg=axcolor)
-#radio = RadioButtons(rax, ('red', 'blue', 'green'), active=0)
-
--- perigee_map.py
+++ perigee_map.py
@@ -89,7 +89,7 @@ def get_att_vecs(ngrid=100):
                          -np.cos(phi) * sin_theta])
     return theta, phi, att_vecs, x, y
 
-def calc_perigee_map(start='2010:116:00:00:00', stop='2010:118:00:00:00', radzone_dur=12, ngrid=100):
+def calc_perigee_map(start='2010:114:20:00:00', stop='2010:117:16:00:00', ngrid=50):
     # Get orbital ephemeris in requested time range
     print ('Fetching ephemeris')
     objs = ('orbit', 'lunar', 'solar')
@@ -99,7 +99,8 @@ def calc_perigee_map(start='2010:116:00:00:00', stop='2010:118:00:00:00', radzon
              for axis in axes]
     ephems = fetch.MSIDset(msids, start, stop)
     times = ephems[msids[0]].times.copy()
-    if any(len(ephems[msid].vals) != len(ephems[msids[0]].vals) for msid in msids):
+    n_ephem = len(ephems[msids[0]].vals)
+    if any(len(ephems[msid].vals) != n_ephem for msid in msids):
         raise ValueError('Unexpected mismatch in ephemeris telemetry sampling')
 
     ephem_xyzs = {}
@@ -107,36 +108,25 @@ def calc_perigee_map(start='2010:116:00:00:00', stop='2010:118:00:00:00', radzon
         msids = ('{0}ephem1_{1}'.format(obj, axis) for axis in axes)
         ephem_xyzs[obj] = np.array([ephems[msid].vals for msid in msids])
 
-    # Find index and time of perigee, then take only times within "radzone" interval
-    orbit_rs = np.sqrt((ephem_xyzs['orbit']**2).sum(0))
-    i_perigee = np.argmin(orbit_rs)
-    time_perigee = times[i_perigee]
-    ok = abs(times - time_perigee) < radzone_dur * 3600.0 / 2
-    times = times[ok]
-    for obj in objs:
-        ephem_xyzs[obj] = ephem_xyzs[obj][:, ok]
-    n_ephem = len(times)
-    
-    # Get the sun position at perigee
-    sun_ra, sun_dec = Ska.Sun.position(time_perigee)
-    sun_eci = Ska.quatutil.radec2eci(sun_ra, sun_dec)
-
-    # Calculate a grid of attitude vectors centered on the anti-sun line and extending
-    # from sun-pitch=180 (center) to sun-pitch=45 (edge).  
+    illums = np.zeros((n_ephem, ngrid, ngrid), dtype=np.float32)
     antisun, xs, ys = get_antisun_grid(ngrid)
     antisun_pitches, phis = antisun.img2polar(xs, ys)
-    att_vecs = antisun.img2eci(xs, ys, sun_eci)
-    ras, decs = Ska.quatutil.eci2radec(att_vecs)
-    
-    illums = np.zeros((n_ephem, ngrid, ngrid), dtype=np.float32)
+
     for i_ephem, ephem_xyz in enumerate(ephem_xyzs['orbit'].transpose()):
+        # Calculate a grid of attitude vectors centered on the anti-sun line and extending
+        # from sun-pitch=180 (center) to sun-pitch=45 (edge).  
+        sun_eci = ephem_xyzs['solar'][:, i_ephem] - ephem_xyzs['orbit'][:, i_ephem]
+        att_vecs = antisun.img2eci(xs, ys, sun_eci)
+        ras, decs = Ska.quatutil.eci2radec(att_vecs)
+
         print i_ephem, n_ephem, ephem_xyz
         for iy in range(ngrid):
             for ix in range(ngrid):
                 i_vec = iy * ngrid + ix
-                if antisun_pitches[i_vec] < 135:
+                orbit_r = np.sqrt(np.sum(ephem_xyzs['orbit'][:, i_ephem]**2))
+                if antisun_pitches[i_vec] < 135 and orbit_r < 50000e3:
                     ra, dec = ras[i_vec], decs[i_vec]
-                    roll = Ska.Sun.nominal_roll(ra, dec, time_perigee)
+                    roll = Ska.Sun.nominal_roll(ra, dec, times[i_ephem])
                     _, att_illums, _ = taco2.calc_earth_vis(ephem_xyz, [ra, dec, roll], max_reflect=5)
                     illum = sum(att_illums)
                 else:
@@ -153,7 +143,6 @@ def calc_perigee_map(start='2010:116:00:00:00', stop='2010:118:00:00:00', radzon
                 ras=ras,
                 decs=decs,
                 ephem_xyzs=ephem_xyzs,
-                orbit_rs=orbit_rs,
                 sun_eci=sun_eci,
                 )
 
@@ -161,5 +150,9 @@ if 0 and __name__ == '__main__':
     opt, args = get_options()
     xs, ys, times, illums = calc_perigee_map(opt.start, opt.stop, opt.radzone_dur)
     
-out = calc_perigee_map(ngrid=50,radzone_dur=4)
-pickle.dump(out, open('cube3.pkl', 'w'))
+out = calc_perigee_map()
+try:
+    filename = sys.argv[1]
+except:
+    filename = 'cube.pkl'
+pickle.dump(out, open(filename, 'w'))
