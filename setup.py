e441bb902cb4a9d12663a3ba2c3833c75a645ee7
Tom Aldcroft <taldcroft@gmail.com>
2018-03-12 14:45:42 -0400

    Move scripts used during development to dev directory

--- setup.py
+++
@@ -1,9 +0,0 @@
-# Licensed under a 3-clause BSD style license - see LICENSE.rst
-from distutils.core import setup
-setup(name='taco',
-      version='0.1.1',
-      description='Calculate Earth illumination on the ACIS radiatiator',
-      author='Tom Aldcroft',
-      author_email='aldcroft@head.cfa.harvard.edu',
-      py_modules=['acis_rad_illum', 'taco', 'plot_geom'],
-      )
