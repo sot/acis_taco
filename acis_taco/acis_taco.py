f6bbe78e25905d5a1f3110b44a3a0468fb7311c2
John ZuHone <jzuhone@gmail.com>
2018-05-18 10:52:03 -0400

    Replace numpy -> np, put the ugly encode hack into a function

--- acis_taco/acis_taco.py
+++ acis_taco/acis_taco.py
@@ -7,7 +7,7 @@ import hashlib
 import functools
 
 from Quaternion import Quat
-import numpy
+import numpy as np
 from numpy.random import RandomState
 
 prng = RandomState()
@@ -37,6 +37,8 @@ def set_random_salt(salt):
     global _RANDOM_SALT
     _RANDOM_SALT = salt
 
+def _encode_data(val):
+    return np.array_str(np.array(val, ndmin=1), precision=10).encode("utf8")
 
 def _make_reproducible(func):
     @functools.wraps(func)
@@ -45,9 +47,9 @@ def _make_reproducible(func):
             md5 = hashlib.md5()
             md5.update(repr(_RANDOM_SALT).encode("utf8"))
             for val in args:
-                md5.update(numpy.array_str(numpy.array(val, ndmin=1), precision=10).encode("utf8"))
+                md5.update(_encode_data(val))
             for val in kwargs.values():
-                md5.update(numpy.array_str(numpy.array(val, ndmin=1), precision=10).encode("utf8"))
+                md5.update(_encode_data(val))
             digest = md5.hexdigest()
             seed = int(digest[:7], 16)
             prng.seed(seed)
@@ -59,10 +61,10 @@ def _make_reproducible(func):
 
 def make_taco():
     """Define geometry for ACIS radiator sun-shade (aka the space taco) in mm."""
-    y_pnts = numpy.array([-689.0, -333, -63, 553, 689]) + TACO_Y_OFF
-    z_pnts = numpy.array([0.0, 777, 777, 421, 0]) - RAD_Z_OFF
+    y_pnts = np.array([-689.0, -333, -63, 553, 689]) + TACO_Y_OFF
+    z_pnts = np.array([0.0, 777, 777, 421, 0]) - RAD_Z_OFF
 
-    y_edge = numpy.arange(max(y_pnts))
+    y_edge = np.arange(max(y_pnts))
     z_edge = interpolate(z_pnts, y_pnts, y_edge)
 
     return z_edge
@@ -94,9 +96,9 @@ def calc_earth_vis(p_chandra_eci,
     q_att = Quat(chandra_att) 
     
     # For T = attitude transformation matrix then p_body = T^-1 p_eci
-    p_earth_body = numpy.dot(q_att.transform.transpose(), -numpy.array(p_chandra_eci))
+    p_earth_body = np.dot(q_att.transform.transpose(), -np.array(p_chandra_eci))
 
-    illum = numpy.zeros(max_reflect+1)
+    illum = np.zeros(max_reflect+1)
     out_rays = []
 
     # If Earth disk is entirely below the "horizon" of the ACIS radiator then illum=0
@@ -105,11 +107,11 @@ def calc_earth_vis(p_chandra_eci,
 
     # Quaternion to transform x-axis to the body-earth vector
     q_earth = quat_x_v2(p_earth_body)
-    open_angle = numpy.arcsin(RAD_EARTH / numpy.sqrt(numpy.sum(p_earth_body**2)))
+    open_angle = np.arcsin(RAD_EARTH / np.sqrt(np.sum(p_earth_body**2)))
     
     rays, earth_solid_angle = sphere_rand(open_angle)
     n_rays = len(rays)
-    rays_to_earth = numpy.dot(q_earth.transform, rays.transpose()).transpose()  # shape (n_rays, 3)
+    rays_to_earth = np.dot(q_earth.transform, rays.transpose()).transpose()  # shape (n_rays, 3)
 
     # Accept only rays with a positive Z component and make sure no X component is < 1e-6
     rays_to_earth = rays_to_earth[rays_to_earth[:, 2] > 0.0, :]
@@ -118,11 +120,11 @@ def calc_earth_vis(p_chandra_eci,
         rays_to_earth = rays_to_earth[:-1, :]
         n_rays_to_earth -= 1
 
-    vis = numpy.zeros((max_reflect+1, n_rays))
+    vis = np.zeros((max_reflect+1, n_rays))
     if len(rays_to_earth) == 0:
         return vis, illum, out_rays
 
-    rays_to_earth_x = numpy.abs(rays_to_earth[:, 0])
+    rays_to_earth_x = np.abs(rays_to_earth[:, 0])
     rays_to_earth_x[rays_to_earth_x < 1e-6] = 1e-6
 
     # Main ray-trace loop.  Calculate ray visibility for an increasing number
@@ -134,9 +136,9 @@ def calc_earth_vis(p_chandra_eci,
 
     rad_x = prng.uniform(low=0.0, high=215.9, size=n_rays_to_earth // 2)
     rad_y = prng.uniform(low=-241.3, high=241.3, size=n_rays_to_earth // 2) + TACO_Y_OFF
-    rad_x = numpy.append(rad_x, -rad_x)
-    rad_y = numpy.append(rad_y, rad_y)
-    rad_z = numpy.zeros(n_rays_to_earth)
+    rad_x = np.append(rad_x, -rad_x)
+    rad_y = np.append(rad_y, rad_y)
+    rad_z = np.zeros(n_rays_to_earth)
     rays_x = rays_to_earth_x
     rays_y = rays_to_earth[:, 1]
     rays_z = rays_to_earth[:, 2]
@@ -148,7 +150,7 @@ def calc_earth_vis(p_chandra_eci,
         dx = (taco_x - rad_x) / rays_x
 
         # Find rays that intersect shade within Y limits of the shade (0, N_TACO mm)
-        ray_taco_iys = numpy.array(rad_y + rays_y * dx, dtype=numpy.int)
+        ray_taco_iys = np.array(rad_y + rays_y * dx, dtype=np.int)
         y_ok = (ray_taco_iys >= 0) & (ray_taco_iys < N_TACO)
         y_not_ok = ~y_ok
         ray_taco_iys[y_not_ok] = 0
@@ -161,7 +163,7 @@ def calc_earth_vis(p_chandra_eci,
         # Calculate the delta-visibility for good rays (cos(incidence_angle) = Z component)
         d_vis = rays_z[y_z_ok] * REFLECT_ATTEN**refl
         # vis[refl, rays_i[y_z_ok]] += d_vis
-        illum[refl] += earth_solid_angle * numpy.sum(d_vis) / n_rays
+        illum[refl] += earth_solid_angle * np.sum(d_vis) / n_rays
 
         blocked = ~y_z_ok
         rays_x = rays_x[blocked]
@@ -187,7 +189,7 @@ def interpolate(yin, xin, xout):
     """
     lenxin = len(xin)
 
-    i1 = numpy.searchsorted(xin, xout)
+    i1 = np.searchsorted(xin, xout)
     i1[ i1==0 ] = 1
     i1[ i1==lenxin ] = lenxin-1
 
@@ -199,20 +201,20 @@ def interpolate(yin, xin, xout):
     return (xout - x0) / (x1 - x0) * (y1 - y0) + y0
 
 def norm(vec):
-    return vec / numpy.sqrt(numpy.sum(vec**2))
+    return vec / np.sqrt(np.sum(vec**2))
 
 def quat_x_v2(v2):
     """Generate quaternion that rotates X-axis into v2 by the shortest path"""
-    x = numpy.array([1.,0,0])
-    v2 = norm(numpy.array(v2))
-    dot = numpy.dot(x, v2)
+    x = np.array([1.,0,0])
+    v2 = norm(np.array(v2))
+    dot = np.dot(x, v2)
     if abs(dot) > 1-1e-8:
-        x = norm(numpy.array([1., 0., 1e-5]))
-        dot = numpy.dot(v2, x)
-    angle = numpy.arccos(dot)
-    axis = norm(numpy.cross(x, v2))
-    sin_a = numpy.sin(angle / 2)
-    cos_a = numpy.cos(angle / 2)
+        x = norm(np.array([1., 0., 1e-5]))
+        dot = np.dot(v2, x)
+    angle = np.arccos(dot)
+    axis = norm(np.cross(x, v2))
+    sin_a = np.sin(angle / 2)
+    cos_a = np.cos(angle / 2)
     return Quat([axis[0] * sin_a,
                             axis[1] * sin_a,
                             axis[2] * sin_a,
@@ -229,7 +231,7 @@ def sphere_grid(ngrid, open_angle):
     
     grid_area = 2*pi*(1-cos(open_angle))
     if ngrid <= 1:
-        return numpy.array([[1., 0., 0.]]), grid_area
+        return np.array([[1., 0., 0.]]), grid_area
 
     gridsize = sqrt(grid_area / ngrid)
 
@@ -253,7 +255,7 @@ def sphere_grid(ngrid, open_angle):
             grid.append((sin(dec), sin(ra) * cos(dec), cos(ra) * cos(dec)))
 
     # (x, y, z) = zip(*grid)  (python magic)
-    return numpy.array(grid), grid_area
+    return np.array(grid), grid_area
 
 
 @_make_reproducible
@@ -269,7 +271,7 @@ def sphere_rand(open_angle, min_ngrid=100, max_ngrid=10000):
     xmin = cos(open_angle)
     grid_area = 2*pi*(1-xmin)
 
-    idx_xmin = numpy.searchsorted(SPHERE_X, xmin)
+    idx_xmin = np.searchsorted(SPHERE_X, xmin)
     if N_SPHERE - idx_xmin < min_ngrid:
         idx_xmin = N_SPHERE - min_ngrid
 
@@ -283,11 +285,11 @@ def sphere_rand(open_angle, min_ngrid=100, max_ngrid=10000):
 def random_hemisphere(nsample):
     x = prng.uniform(low=0.3, high=1.0, size=nsample)
     x.sort()                    # x is not random
-    t = 2*numpy.pi * prng.uniform(nsample)
-    r = numpy.sqrt(1-x**2)
-    z = r * numpy.cos(t)
-    y = r * numpy.sin(t)
-    return numpy.array([x, y, z]).transpose()
+    t = 2*np.pi * prng.uniform(nsample)
+    r = np.sqrt(1-x**2)
+    z = r * np.cos(t)
+    y = r * np.sin(t)
+    return np.array([x, y, z]).transpose()
 
 
 RAD_EARTH = 6371e3
