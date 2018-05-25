94ee44b5052ead316f12f63be6a6eff580409470
John ZuHone <jzuhone@gmail.com>
2018-05-25 17:48:14 -0400

    Setting this up for pytest

--- acis_taco/__init__.py
+++ acis_taco/__init__.py
@@ -1,3 +1,10 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 from .acis_taco import *
 from .version import version as __version__
+
+def test(*args, **kwargs):
+    """
+    Run py.test unit tests.
+    """
+    import testr
+    return testr.test(*args, **kwargs)
--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 from setuptools import setup
-from acis_taco.version import version
+from acis_taco import __version__
+try:
+    from testr.setup_helper import cmdclass
+except ImportError:
+    cmdclass = {}
 
 setup(name='acis_taco',
-      author = 'Tom Aldcroft',
+      author='Tom Aldcroft',
       description='ACIS Earth Solid Angle package',
-      author_email = 'aldcroft@head.cfa.harvard.edu',
-      url = 'http://cxc.cfa.harvard.edu/mta/ASPECT/tool_doc/taco/',
-      version=version,
+      author_email='aldcroft@head.cfa.harvard.edu',
+      url='http://cxc.cfa.harvard.edu/mta/ASPECT/tool_doc/taco/',
+      version=__version__,
       zip_safe=False,
       packages=['acis_taco'],
+      cmdclass=cmdclass,
       )
 
 
